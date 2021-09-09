69b566f96cdf9e32723633b7af336de8bfa07dd9
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-09-09 21:26:19 +0300

    Update chunk.c

--- src/chunk.c
+++ src/chunk.c
@@ -1,83 +1,94 @@
-#include <stdlib.h>
-
+#include <stdio.h>
 #include "chunk.h"
-#include "common.h"
 #include "memory.h"
 
-void init_chunk(chunk* c) {
-  c->count = 0;
-  c->capacity = 0;
-  c->code = NULL;
-  c->lines = NULL;
-  c->lines_count = 0;
-  c->lines_capacity = 0;
-  init_value_array(&c->constants);
+void initChunk(Chunk *chunk)
+{
+    chunk->count = 0;
+    chunk->capacity = 0;
+    chunk->code = NULL;
+    chunk->lineCount = 0;
+    chunk->lineCapacity = 0;
+    chunk->lines = NULL;
+    // Initializing the constants struct
+    initValueArray(&chunk->constants);
 }
 
-void encode_line(chunk* c, int line) {
-  if (c->lines && c->lines[c->lines_count-1] == line) {
-    c->lines[c->lines_count-2] += 1;
-    return;
-  }
+void writeChunk(Chunk* chunk, uint8_t byte, int line)
+{
+    if (chunk->capacity < chunk->count + 1) {
+        int oldCapacity = chunk->capacity;
+        chunk->capacity = GROW_CAPACITY(oldCapacity);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    }
+    chunk->code[chunk->count++] = byte;
 
-  if (c->lines_capacity < c->lines_count + 1) {
-    int oldc = c->lines_capacity;
-    c->lines_capacity = GROW_CAPACITY(c->lines_capacity);
-    c->lines = GROW_ARRAY(c->lines, int, oldc, c->lines_capacity);
-  }
-  c->lines[c->lines_count] = 1;
-  c->lines[c->lines_count+1] = line;
-  c->lines_count += 2;
+    compress(chunk, line);
 }
 
-void write_chunk(chunk* c, uint8_t byte, int line) {
-  if (c->capacity < c->count + 1) {
-    int oldc = c->capacity;
-    c->capacity = GROW_CAPACITY(oldc);
-    c->code = GROW_ARRAY(c->code, uint8_t, oldc, c->capacity);
-  }
+void compress(Chunk* chunk, int line)
+{
+    // Allocation space for the lines struct
+    if (chunk->lineCapacity < chunk->lineCount + 1) {
+        int oldCapacity = chunk->lineCapacity;
+        chunk->lineCapacity = GROW_CAPACITY(oldCapacity);
+        chunk->lines = GROW_ARRAY(lineTrace, chunk->lines, oldCapacity, chunk->lineCapacity);
+    }
 
-  c->code[c->count] = byte;
-  encode_line(c, line);
-  c->count++;
+    if (chunk->lineCount == line - 1) {
+        chunk->lines[chunk->lineCount].line = line;
+        chunk->lines[chunk->lineCount].offset = chunk->count;
+    } else {
+        chunk->lineCount++;
+        chunk->lines[chunk->lineCount].line = line;
+        chunk->lines[chunk->lineCount].offset = chunk->count;
+    }
 }
 
-void write_constant(chunk* c, value v, int line) {
-  int offs = add_constant(c, v);
-
-  if (c->capacity < c->count + 4) {
-    int oldc = c->capacity;
-    c->capacity = GROW_CAPACITY(oldc);
-    c->code = GROW_ARRAY(c->code, uint8_t, oldc, c->capacity);
-  }
-
-  if (offs < 256) {
-    c->code[c->count] = OP_CONSTANT;
-    encode_line(c, line);
-    c->code[c->count+1] = offs;
-    encode_line(c, line);
-    c->count += 2;
-  } else {
-    c->code[c->count] = OP_CONSTANT_LONG;
-    encode_line(c, line);
-    c->code[c->count+1] = offs & 0xff;
-    encode_line(c, line);
-    c->code[c->count+2] = (offs & 0xff00) >> 8;
-    encode_line(c, line);
-    c->code[c->count+3] = (offs & 0xff0000) >> 16;
-    encode_line(c, line);
-    c->count += 4;
-  }
+void freeChunk(Chunk *chunk)
+{
+    FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(lineTrace , chunk->lines, chunk->lineCapacity);
+    // Freeing the constants struct
+    freeValueArray(&chunk->constants);
+    initChunk(chunk);
 }
 
-void free_chunk(chunk* c) {
-  FREE_ARRAY(uint8_t, c->code, c->capacity);
-  FREE_ARRAY(int, c->lines, c->capacity);
-  free_value_array(&c->constants);
-  init_chunk(c);
+int addConstant(Chunk* chunk, Value value)
+{
+    writeValueArray(&chunk->constants, value);
+    // Returns the index of the instruction
+    return chunk->constants.count - 1;
 }
 
-int add_constant(chunk* c, value value) {
-  write_value_array(&c->constants, value);
-  return c->constants.count - 1;
+int getLine(Chunk* chunk, int offset)
+{
+    bool flag = true;
+    int i;
+
+    for (i = 0; i < chunk->lineCount+1 && flag; i++) {
+        if (offset < chunk->lines[i].offset)
+            flag = false;
+    }
+    return chunk->lines[i-1].line;
 }
+
+void writeConstant(Chunk* chunk, Value value, int line)
+{
+    // Adding the value to the constants array
+    int index = addConstant(chunk, value);
+
+    // If the index value is less than 256 (int 8 bits range) no special work is done
+    if (index < 256) {
+        writeChunk(chunk, OP_CONSTANT, line);
+        writeChunk(chunk, (uint8_t) index, line);
+    } else {
+        // If the index is larger than 256, we store it using little-endian representation.
+        // We are performing & on the lower 8 bits to get the value, and store in the line.
+        writeChunk(chunk, OP_CONSTANT_LONG, line);
+        writeChunk(chunk, (uint8_t) (index & 0xff), line);
+        writeChunk(chunk, (uint8_t) ((index >> 8) & 0xff), line);
+        writeChunk(chunk, (uint8_t) ((index >> 16) & 0xff), line);
+    }
+}
+
